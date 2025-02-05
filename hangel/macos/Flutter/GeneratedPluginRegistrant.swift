c3b3baa85f3c081a04d691f0de2f5ce7b5bffaf8
hangelapp <hangelapp@gmail.com>
2025-02-05 03:28:10 +0100

    [UPDATE] version updated

--- hangel/macos/Flutter/GeneratedPluginRegistrant.swift
+++ hangel/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,7 +18,7 @@ import in_app_review
 import package_info_plus
 import path_provider_foundation
 import share_plus
-import sqflite_darwin
+import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
