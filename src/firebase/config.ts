ac80939b3e917f0559de78aea81ab7a4b536128b
İsmail Hilmi ADIGÜZEL <ismailhilmi@hangel.org>
2026-02-09 01:14:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7575710471-3f302",
+  "appId": "1:203236927636:web:057e04e2a98fb7efe5ec81",
+  "apiKey": "AIzaSyC6i9E1jX1JUUrBo0lZhNtch20Twwtr6qk",
+  "authDomain": "studio-7575710471-3f302.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "203236927636"
+};
